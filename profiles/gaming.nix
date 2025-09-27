cf66b4b1c3bb53aaefa0b92d8ff77ad6c8e23bee
V3RM1N <v3rm1n0@proton.me>
2025-09-27 10:35:19 +0200

    reafactor: Switched to discord canary build for better wayland

--- profiles/gaming.nix
+++ profiles/gaming.nix
@@ -10,7 +10,7 @@ with lib;
 let
   cfg = config.gaming;
   defaultPackages = with pkgs; [
-    (discord.override {
+    (discord-canary.override {
       withOpenASAR = false;
       withVencord = true;
     })
