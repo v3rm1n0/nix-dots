fa3a3a3d144ae9101ffc6a5cc2c33006799cce5f
V3RM1N <v3rm1n0@proton.me>
2025-10-02 13:39:16 +0200

    feat: Add new nix logo (png) refactor: rewrote fastfetch config to use image instead of ascii (wezterm image support)

--- assets/default.nix
+++ assets/default.nix
@@ -6,7 +6,7 @@ in
   home-manager.users.${username} = _: {
     home.file = {
       ".config/backgrounds".source = ./wallpapers;
-      ".config/nixlogo.svg".source = ./logo/nix-snowflake.svg;
+      ".config/nixlogo.png".source = ./logo/nix-snowflake.png;
     };
   };
 }
