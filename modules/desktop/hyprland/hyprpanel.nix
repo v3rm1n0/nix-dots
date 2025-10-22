8becb43c467452c9fabb53bfca9bb0696e00aa41
V3RM1N <v3rm1n0@proton.me>
2025-10-22 13:29:54 +0200

    chore: Updated hyprpanel config

--- modules/desktop/hyprland/hyprpanel.nix
+++ modules/desktop/hyprland/hyprpanel.nix
@@ -18,9 +18,9 @@ in
             left = [
               "dashboard"
               "workspaces"
-              "windowtitle"
+              "cava"
             ];
-            middle = [ "media" ];
+            middle = [ "windowtitle" ];
             right = [
               "volume"
               "kbinput"
@@ -36,9 +36,9 @@ in
             left = [
               "dashboard"
               "workspaces"
-              "windowtitle"
+              "cava"
             ];
-            middle = [ "media" ];
+            middle = [ "windowtitle" ];
             right = [
               "volume"
               "kbinput"
@@ -54,21 +54,23 @@ in
         bar.launcher.autoDetectIcon = true;
         bar.windowtitle.icon = false;
         bar.workspaces.show_numbered = true;
-        bar.workspaces.workspaces = 10;
+        bar.workspaces.workspaces = 2;
         menus.clock.time.military = true;
         menus.clock.weather.key = "/home/${username}/.config/apikey.json";
         menus.clock.weather.location = "Viersen";
         menus.clock.weather.unit = "metric";
-        theme.bar.buttons.background_opacity = "0";
+        theme.bar.buttons.background_opacity = "100";
+        theme.bar.buttons.padding_x = "0.3rem";
+        theme.bar.buttons.padding_y = "0rem";
         theme.bar.floating = true;
+        theme.bar.margin_bottom = "0.5em";
+        theme.bar.margin_sides = "1em";
         theme.bar.menus.opacity = "90";
         theme.bar.outer_spacing = "0.4em";
-        theme.bar.transparent = true;
+        theme.bar.transparent = false;
         theme.font.size = "0.7rem";
         theme.matugen = false;
-        theme.matugen_settings.scheme_type = "fidelity";
         wallpaper.enable = false;
-        wallpaper.image = "/home/${username}/.config/backgrounds/${wallpaper}";
       };
     };
   };
