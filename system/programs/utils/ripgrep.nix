b26a24231344613f9ccb4536269475b4b8d7ad8d
V3RM1N <v3rm1n0@proton.me>
2025-10-28 11:18:45 +0100

    feat(ripgrep): configure session variables and update .ripgreprc settings

--- system/programs/utils/ripgrep.nix
+++ system/programs/utils/ripgrep.nix
@@ -4,8 +4,21 @@
   pkgs,
   ...
 }:
+let
+  username = config.userOptions.username;
+in
 {
+  environment.sessionVariables = {
+    RIPGREP_CONFIG_PATH = "$HOME/.ripgreprc";
+  };
   environment.systemPackages = with pkgs; [
     ripgrep
   ];
+
+  home-manager.users.${username} = {
+    home.file.".ripgreprc".text = ''
+      --glob=!.git/*
+      --glob=!flake.lock
+    '';
+  };
 }
