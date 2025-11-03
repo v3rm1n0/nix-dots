3062f50a28f5f9569adff313b6c5566fb367ffc9
V3RM1N <v3rm1n0@proton.me>
2025-11-03 11:15:44 +0100

    feat: Added container extensions for firefox

--- modules/applications/browsing/firefox.nix
+++ modules/applications/browsing/firefox.nix
@@ -77,20 +77,12 @@
           OverridePostUpdatePage = "";
           SearchBar = "unified";
 
-          # ─── Search engine config ─────────────────────────────────────────────────────
-          # Broken on ESR
-          /*
-            SearchEngines = {
-              Default = "Startpage";
-            };
-          */
-
           # ─── Extensions ───────────────────────────────────────────────────────────────
           ExtensionSettings = {
             "*".installation_mode = "allowed";
             "addon@darkreader.org" = {
               install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
-              installation_mode = "force_installed";
+              installation_mode = "normal_installed";
             };
             # Bitwarden:
             "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
@@ -100,11 +92,26 @@
             # DeArrow:
             "deArrow@ajay.app" = {
               install_url = "https://addons.mozilla.org/firefox/downloads/latest/dearrow/latest.xpi";
+              installation_mode = "normal_installed";
+            };
+            # Facebook Container
+            "@contain-facebook" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/facebook-container/latest.xpi";
+              installation_mode = "force_installed";
+            };
+            # Google containers (not official)
+            "@contain-google" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/google-container/latest.xpi";
               installation_mode = "force_installed";
             };
             # Meterial Icons for GitHub:
             "{eac6e624-97fa-4f28-9d24-c06c9b8aa713}" = {
               install_url = "https://addons.mozilla.org/firefox/downloads/latest/material-icons-for-github/latest.xpi";
+              installation_mode = "normal_installed";
+            };
+            # Multi Account Containers
+            "@testpilot-containers" = {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/multi-account-containers/latest.xpi";
               installation_mode = "force_installed";
             };
             # Privacy Badger:
@@ -115,12 +122,12 @@
             # Qwant Search:
             "qwant-search-firefox@qwant.com" = {
               install_url = "https://addons.mozilla.org/firefox/downloads/latest/qwant-the-search-engine/latest.xpi";
-              installation_mode = "force_installed";
+              installation_mode = "normal_installed";
             };
             # Return YouTube Dislike:
             "{762f9885-5a13-4abd-9c77-433dcd38b8fd}" = {
               install_url = "https://addons.mozilla.org/firefox/downloads/latest/return-youtube-dislikes/latest.xpi";
-              installation_mode = "force_installed";
+              installation_mode = "normal_installed";
             };
             # uBlock Origin:
             "uBlock0@raymondhill.net" = {
@@ -130,7 +137,7 @@
             # YouTube High Definition:
             "{7b1bf0b6-a1b9-42b0-b75d-252036438bdc}" = {
               install_url = "https://addons.mozilla.org/firefox/downloads/latest/youtube-high-definition/latest.xpi";
-              installation_mode = "force_installed";
+              installation_mode = "normal_installed";
             };
           };
         };
