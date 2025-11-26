1908b08c820fbf319bda2180014d104cb46f414f
V3RM1N <v3rm1n0@proton.me>
2025-11-26 15:29:45 +0100

    feat: Added sshkey to gnupg agent

--- modules/security/gnupg/default.nix
+++ modules/security/gnupg/default.nix
@@ -13,19 +13,33 @@ in
   };
 
   config = lib.mkIf config.securityModule.gpg.enable {
-    programs.gnupg = {
-      agent.enable = true;
-      agent.enableSSHSupport = true;
-    };
-
-    environment.systemPackages = with pkgs; [
-      pinentry-curses
-      pinentry-gnome3
-    ];
+    #programs.gnupg = {
+    #  agent.enable = true;
+    #  agent.enableSSHSupport = true;
+    #};
+    #
+    #environment.systemPackages = with pkgs; [
+    #  pinentry-curses
+    #  pinentry-gnome3
+    #];
 
     home-manager.users.${username} = _: {
+        programs.gpg.enable = true;
+
+        services.gpg-agent = {
+          enable = true;
+          enableSshSupport = true;
+          pinentry.package = pkgs.pinentry-gnome3;
+          
+          defaultCacheTtlSsh = 14400;
+          maxCacheTtlSsh = 14400;
+
+          sshKeys = [
+            "2EAF5EEFD4334DD0130D5158FED38D4505C78DAF"
+          ];
+        };
       home.file = {
-        ".gnupg/gpg-agent.conf".source = ./gpg-agent.conf;
+        #".gnupg/gpg-agent.conf".source = ./gpg-agent.conf;
       };
     };
   };
