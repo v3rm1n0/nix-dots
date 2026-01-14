1b0ff65c1dcb490d5e04a52b4fab2fe889d06875
V3RM1N <v3rm1n0@proton.me>
2026-01-14 15:25:45 +0100

    chore: enable open option for NVIDIA hardware configuration

--- system/hardware/graphics/default.nix
+++ system/hardware/graphics/default.nix
@@ -28,9 +28,9 @@ in
         modesetting.enable = true;
         powerManagement.enable = true;
         powerManagement.finegrained = false;
-        open = false;
+        open = true;
         nvidiaSettings = true;
-        package = config.boot.kernelPackages.nvidiaPackages.stable;
+        #package = config.boot.kernelPackages.nvidiaPackages.stable;
       };
     })
 
