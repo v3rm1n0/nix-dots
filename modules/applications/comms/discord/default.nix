58a03136d17406bd51d1c41efe93d7fc44b7886f
V3RM1N <mail@v3rm1n.dev>
2026-03-31 11:24:57 +0200

    feat: add plugins for nixcord

--- modules/applications/comms/discord/default.nix
+++ modules/applications/comms/discord/default.nix
@@ -12,6 +12,39 @@
           enable = true;
           discord.enable = lib.mkDefault false;
           equibop.enable = lib.mkDefault true;
+          config = {
+            plugins = {
+              betterFolders = {
+                enable = true;
+                closeOthers = true;
+                sidebar = false;
+              };
+              betterSessions.enable = true;
+              callTimer = {
+                enable = true;
+                allCallTimers = true;
+              };
+              declutter.enable = true;
+              fakeNitro.enable = true;
+              gameActivityToggle.enable = true;
+              gitHubRepos.enable = true;
+              messageLatency.enable = true;
+              moreUserTags.enable = true;
+              musicControls.enable = true;
+              permissionsViewer.enable = true;
+              platformIndicators.enable = true;
+              quoter.enable = true;
+              roleColorEverywhere.enable = true;
+              sendTimestamps.enable = true;
+              showHiddenChannels.enable = true;
+              showHiddenThings.enable = true;
+              showMeYourName.enable = true;
+              youtubeAdblock.enable = true;
+            };
+            themeLinks = [
+              "https://raw.githubusercontent.com/shvedes/discord-gruvbox/refs/heads/main/gruvbox-dark.theme.css"
+            ];
+          };
         };
       };
     };
