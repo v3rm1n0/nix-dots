3b7773e42a9c2eb8ec503b41cd0deff93dfa6ff1
V3RM1N <mail@v3rm1n.dev>
2026-05-06 11:10:53 +0200

    security: replace initialPassword with hashed password

--- users/v3rm1n/default.nix
+++ users/v3rm1n/default.nix
@@ -16,7 +16,8 @@
       users.users.${username} = {
         shell = pkgs.zsh;
         isNormalUser = true;
-        initialPassword = "temp123";
+        hashedPassword = "$6$TSeuDdaiycwV2p9R$SfYPYi5lKha0PLWOqoCXTJW8/SthhJ3R99Hfvo8g5AT5hR3BZIUTmXNmxU03DyJNrSu/yh6SDwkbEXIOOlETO.";
+        initialHashedPassword = "$6$MtOmPT5eXBAUeObZ$bgsp7GS17AGGjJQlmmoIx8NHfhEQVgU91hOqtHe9p6nOedPWMxjnwtDHMNJjReh0CYJA755srEaqQ9ygDYBRN1";
         extraGroups = [
           "docker"
           "wheel"
