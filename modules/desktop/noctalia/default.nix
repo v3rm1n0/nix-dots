de909b40306fbd7b2bebc108961c42ae1f5b1935
V3RM1N <mail@v3rm1n.dev>
2026-05-13 16:04:10 +0200

    refactor: turn noctalia in wrapper

--- modules/desktop/noctalia/default.nix
+++ modules/desktop/noctalia/default.nix
@@ -3,7 +3,8 @@
   flake.nixosModules.modulesDesktopNoctalia =
     {
       config,
-      lib,
+      pkgs,
+      self,
       ...
     }:
     let
@@ -12,76 +13,16 @@
     {
       imports = [ inputs.noctalia.nixosModules.default ];
 
+      environment.systemPackages = [
+        self.packages.${pkgs.stdenv.hostPlatform.system}.${
+          if hostName == "Laptop" then "noctalia-shell-laptop" else "noctalia-shell"
+        }
+      ];
+
       hjem.users.${username} = {
         files.".cache/noctalia/wallpapers.json" = {
           text = builtins.toJSON {
             defaultWallpaper = "/home/${username}/.config/backgrounds/${wallpaper}";
-          };
-        };
-
-        files.".config/noctalia/config.json" = {
-          generator = lib.generators.toJSON { };
-          value = {
-            settingsVersion = 63;
-            bar = {
-              barType = "floating";
-              density = "compact";
-              position = "top";
-              showCapsule = false;
-              widgets = {
-                left = [
-                  {
-                    id = "ControlCenter";
-                    useDistroLogo = true;
-                  }
-                  { id = "SystemMonitor"; }
-                  {
-                    id = "Workspace";
-                    labelMode = "none";
-                  }
-                  {
-                    id = "AudioVisualizer";
-                    width = 100;
-                    hideWhenIdle = true;
-                  }
-                  { id = "MediaMini"; }
-                ];
-                center = [
-                  { id = "ActiveWindow"; }
-                ];
-                right = [
-                  { id = "Volume"; }
-                  { id = "Network"; }
-                  { id = "Bluetooth"; }
-                ]
-                ++ lib.optional (hostName == "Laptop") { id = "Battery"; }
-                ++ [
-                  {
-                    id = "KeyboardLayout";
-                    showIcon = false;
-                  }
-                  { id = "Tray"; }
-                  { id = "NotificationHistory"; }
-                  {
-                    formatHorizontal = "HH:mm";
-                    formatVertical = "HH mm";
-                    id = "Clock";
-                    useMonospacedFont = true;
-                    usePrimaryColor = true;
-                  }
-                ];
-              };
-            };
-            colorSchemes.predefinedScheme = "kanagawa";
-            dock.enabled = false;
-            general = {
-              scaleRatio = 0.9;
-              shadowDirection = "top_right";
-            };
-            location = {
-              monthBeforeDay = false;
-              name = "Viersen, Germany";
-            };
           };
         };
       };
