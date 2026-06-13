019c93d17440f5efd09b8a7412c8d712b48e23a9
V3RM1N <mail@v3rm1n.dev>
2026-06-13 12:40:33 +0200

    chore: remove unnecessary proton mail desktop entry

--- modules/applications/comms/default.nix
+++ modules/applications/comms/default.nix
@@ -26,16 +26,6 @@
             teamspeak6-client
             zoom-us
           ];
-
-          files.".local/share/applications/proton-mail.desktop".text = ''
-            [Desktop Entry]
-            Name=Proton Mail
-            Exec=proton-mail --ozone-platform=x11
-            Terminal=false
-            Type=Application
-            Icon=${pkgs.protonmail-desktop}/share/pixmaps/proton-mail.png
-            Categories=Network;Email;
-          '';
         };
       };
     };
