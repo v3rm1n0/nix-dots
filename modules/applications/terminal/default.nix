10eb77d22fa50feb5c61516496cc35c316f32f38
V3RM1N <mail@v3rm1n.dev>
2026-07-02 21:44:40 +0200

    feat: add ghostty rum module

--- modules/applications/terminal/default.nix
+++ modules/applications/terminal/default.nix
@@ -3,7 +3,6 @@ _: {
     {
       config,
       lib,
-      pkgs,
       ...
     }:
     let
@@ -14,36 +13,39 @@ _: {
       options.programs.terminal.enable = lib.mkEnableOption "Enable terminal module";
 
       config = lib.mkIf config.programs.terminal.enable {
-        environment.systemPackages = [ pkgs.ghostty ];
-
-        hjem.users.${username}.files.".config/ghostty/config".text = ''
-          background-opacity = 0.5
-          cursor-style = block
-          cursor-style-blink = false
-          shell-integration = detect
-          font-size = 10
-          background = #${colors.base00}
-          foreground = #${colors.base05}
-          cursor-color = #${colors.base05}
-          selection-background = #${colors.base02}
-          selection-foreground = #${colors.base05}
-          palette = 0=#${colors.base00}
-          palette = 1=#${colors.base08}
-          palette = 2=#${colors.base0B}
-          palette = 3=#${colors.base0A}
-          palette = 4=#${colors.base0D}
-          palette = 5=#${colors.base0E}
-          palette = 6=#${colors.base0C}
-          palette = 7=#${colors.base05}
-          palette = 8=#${colors.base03}
-          palette = 9=#${colors.base08}
-          palette = 10=#${colors.base0B}
-          palette = 11=#${colors.base0A}
-          palette = 12=#${colors.base0D}
-          palette = 13=#${colors.base0E}
-          palette = 14=#${colors.base0C}
-          palette = 15=#${colors.base07}
-        '';
+        hjem.users.${username}.rum.programs.ghostty = {
+          enable = true;
+          settings = {
+            background-opacity = "0.5";
+            cursor-style = "block";
+            cursor-style-blink = false;
+            shell-integration = "detect";
+            font-size = 10;
+            background = "#${colors.base00}";
+            foreground = "#${colors.base05}";
+            cursor-color = "#${colors.base05}";
+            selection-background = "#${colors.base02}";
+            selection-foreground = "#${colors.base05}";
+            palette = [
+              "0=#${colors.base00}"
+              "1=#${colors.base08}"
+              "2=#${colors.base0B}"
+              "3=#${colors.base0A}"
+              "4=#${colors.base0D}"
+              "5=#${colors.base0E}"
+              "6=#${colors.base0C}"
+              "7=#${colors.base05}"
+              "8=#${colors.base03}"
+              "9=#${colors.base08}"
+              "10=#${colors.base0B}"
+              "11=#${colors.base0A}"
+              "12=#${colors.base0D}"
+              "13=#${colors.base0E}"
+              "14=#${colors.base0C}"
+              "15=#${colors.base07}"
+            ];
+          };
+        };
       };
     };
 }
