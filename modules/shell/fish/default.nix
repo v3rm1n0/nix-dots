735071c70970ace7dab3a55ded129ba1dec8243d
V3RM1N <mail@v3rm1n.dev>
2026-07-02 21:44:40 +0200

    refactor: fish wrapper into rum module fixes tide error

--- modules/shell/fish/default.nix
+++ modules/shell/fish/default.nix
@@ -7,13 +7,37 @@
       pkgs,
       ...
     }:
+    let
+      inherit (config.userOptions) username;
+      myAliases = self.lib.commonAliases;
+    in
     {
       options.shell.fish.enable = lib.mkEnableOption "Enable fish Module";
 
       config = lib.mkIf config.shell.fish.enable {
-        programs.fish = {
+        programs.fish.enable = true;
+
+        # Vendored fish plugins are installed system-wide so fish discovers them
+        # via /run/current-system/sw/share/fish/vendor_*. tide provides the prompt
+        # (installing it as a package is what fixes the previously-broken tide).
+        environment.systemPackages = with pkgs.fishPlugins; [
+          tide
+          puffer
+          done
+          bass
+          fzf-fish
+        ];
+
+        hjem.users.${username}.rum.programs.fish = {
           enable = true;
-          package = self.packages.${pkgs.stdenv.hostPlatform.system}.fish;
+          aliases = myAliases;
+          config = ''
+            set -g fish_greeting
+            ${lib.getExe pkgs.any-nix-shell} fish --info-right | source
+            ${lib.getExe pkgs.direnv} hook fish | source
+            ${lib.getExe pkgs.tirith} init --shell fish | source
+            ${lib.getExe pkgs.zoxide} init fish --cmd cd | source
+          '';
         };
       };
     };
--- modules/shell/zsh/default.nix
+++ modules/shell/zsh/default.nix
@@ -8,7 +8,11 @@
       ...
     }:
     let
+      inherit (config.userOptions) username;
       myAliases = self.lib.commonAliases;
+      aliasLines = lib.concatStringsSep "\n" (
+        lib.mapAttrsToList (n: v: "alias ${n}=${lib.escapeShellArg v}") myAliases
+      );
     in
     {
       imports = [ self.nixosModules.modulesShellZshP10k ];
@@ -19,28 +23,34 @@
         environment.shells = [ pkgs.zsh ];
         environment.systemPackages = [ pkgs.fzf ];
 
+        # NixOS base: registers zsh as a login shell, writes /etc/zshrc and sets
+        # up system completion. Interactive config lives in hjem-rum below.
         programs.zsh = {
           enable = true;
-          autosuggestions = {
-            enable = true;
-            strategy = [
-              "completion"
-              "history"
-            ];
-          };
-          syntaxHighlighting.enable = true;
           enableCompletion = true;
-          shellAliases = myAliases;
-          histSize = 10000;
+        };
 
-          interactiveShellInit = ''
+        hjem.users.${username}.rum.programs.zsh = {
+          enable = true;
+          initConfig = ''
             source ${pkgs.zsh-nix-shell}/share/zsh-nix-shell/nix-shell.plugin.zsh
             source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
             source ~/.config/zsh/.p10k.zsh
             POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true
+
+            HISTSIZE=10000
+            SAVEHIST=10000
+
+            ${aliasLines}
+
+            ZSH_AUTOSUGGEST_STRATEGY=(completion history)
+            source ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
             eval "$(tirith init --shell zsh)"
             eval "$(zoxide init --cmd cd zsh)"
             source <(fzf --zsh)
+
+            source ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
           '';
         };
       };
