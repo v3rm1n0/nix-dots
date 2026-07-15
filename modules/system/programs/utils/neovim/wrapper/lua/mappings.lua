b40e97d469182fd55a966793e9698da458b7579d
V3RM1N <mail@v3rm1n.dev>
2026-07-15 16:12:51 +0200

    chore: make nvimtree focus toggleable

--- modules/system/programs/utils/neovim/wrapper/lua/mappings.lua
+++ modules/system/programs/utils/neovim/wrapper/lua/mappings.lua
@@ -6,5 +6,13 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>e", function()
+  local api = require("nvim-tree.api")
+  if vim.bo.filetype == "NvimTree" then
+    vim.cmd("wincmd p") -- go back to previous window
+  else
+    api.tree.focus()
+  end
+end, { desc = "toggle focus nvimtree" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
