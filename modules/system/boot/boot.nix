9b1da71a8b2667e90421f1c3149829e20b87171f
V3RM1N <mail@v3rm1n.dev>
2026-08-22 08:52:26 +0200

    feat: use limine bootloader on Laptop

--- modules/system/boot/boot.nix
+++ modules/system/boot/boot.nix
@@ -1,28 +1,22 @@
 _: {
-  flake.modules.nixos.default =
-    { config, lib, ... }:
-    let
-      inherit (config.userOptions) hostName;
-    in
-    {
-      config = {
-        boot = {
-          loader = {
-            limine = {
-              enable = lib.mkIf (hostName == "Desktop") true;
-              extraEntries = ''
-                /Windows
-                  protocol: efi
-                  path: boot():/EFI/Microsoft/Boot/bootmgfw.efi
-              '';
-              resolution = "1920x1080x32";
-              secureBoot.enable = true;
-            };
-            systemd-boot.enable = lib.mkIf (hostName == "Laptop") true;
-            efi.canTouchEfiVariables = true;
+  flake.modules.nixos.default = {
+    config = {
+      boot = {
+        loader = {
+          limine = {
+            enable = true;
+            extraEntries = ''
+              /Windows
+                protocol: efi
+                path: boot():/EFI/Microsoft/Boot/bootmgfw.efi
+            '';
+            resolution = "1920x1080x32";
+            secureBoot.enable = true;
           };
+          efi.canTouchEfiVariables = true;
         };
-        boot.tmp.cleanOnBoot = true;
       };
+      boot.tmp.cleanOnBoot = true;
     };
+  };
 }
